c6d08ed44dafe0f22e12081bbc64610a2806f03b
Kappxs <montesouzajoao@gmail.com>
2025-09-19 20:57:21 -0300

    Development(Formulario): Adiciona botão, label no formulario e estiliza inputs

--- src/components/AdminForm/index.jsx
+++ src/components/AdminForm/index.jsx
@@ -9,14 +9,24 @@ export default function AdminForm() {
                     <button className="icon-exit">
                         &times;
                     </button>
-                    
+
                     <h3 className="title-form">Cadastrar nova tarefa</h3>
-                    
+
                 </div>
                 <form action="" className="admin-form">
-                    <input type="text" />
-                    <textarea name="description-textarea" id="description-textarea">
-                    </textarea>
+                    <div className="inputs-div">
+
+                        <div className="label-div-title">
+                            <label htmlFor="title-input">Título</label>
+                        </div>
+                        <input type="text" id="title-input" placeholder="Titulo da nova tarefa..." />
+                        <div className="label-div-description"><label htmlFor="description-textarea">Descrição</label></div>
+                        <textarea name="description-textarea" id="description-textarea" placeholder="Descrição da tarefa...">
+                        </textarea>
+                        <div className="button-submit">
+                            <button>Cadastrar</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </main>
