4bc874179071ecf6f295871d02dd9ac1df5a2ac9
Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>
2025-10-10 15:57:29 -0300

    feat(TasksDashboard): add layout da futura tela de dashboard
    Signed-off-by: Wesley Silva <desenvolvimentowesleyleopoldo@gmail.com>

--- src/components/AdminDashboard/index.jsx
+++ src/components/AdminDashboard/index.jsx
@@ -1,25 +1,56 @@
 import { useState } from "react";
-import "./styles.css";
+import "./styles.css"
+import { tasks } from "../../datas/data";
+import TasksAdmin from "../TasksAdmin";
 
 export default function AdminDashboard() {
-    let progressPercent = 1;
+    const [viewMore, setViewMore] = useState(false);
+    const [completed, setCompleted] = useState(Array(tasks.length).fill(false));
+
+    function overFlow() {
+        setViewMore(true);
+    }
+
+    function handleTaskCheck(index, isChecked) {
+        const updated = [...completed];
+        updated[index] = isChecked;
+        setCompleted(updated);
+    }
+
+    const progressPercent = (completed.filter(Boolean).length / tasks.length) * 100;
+
     return (
-        <main className="content-admin">
-            <div className="admin-container">
-                <div className="header-tasks">
-                    <div className="title-tasks">
+        <main className="content-dashboard">
+            <div className="container-main-dashboard">
+                <div className="header-tasks-dashboard">
+                    <div className="title-tasks-dashboard">
                         <h1>Tarefas</h1>
                     </div>
-                        <div className="progress-bar">
-                            <div className="progress-fill" style={{ width: `${progressPercent}%`}}></div>
-                        </div>
+                    <div className="progress-bar-dashboard">
+                        <div className="progress-fill-dashboard" style={{ width: `${progressPercent}%` }}></div>
+                    </div>
+                </div>
+                <div className="tasks-dashboard" style={{ overflowY: viewMore ? "auto" : "hidden" }}>
+
+                    {
+                        tasks.map((task) => (
+                            <TasksAdmin
+                                key={task.id}
+                                task={task.title}
+                                description={task.description}
+                                checked={completed[task.id]}
+                                onCheck={(isChecked) => handleTaskCheck(task.id, isChecked)}
+                            />
+                        ))
+                    }
+
                 </div>
 
-                <div className="tasks">
-                    
+                {tasks.length > 6 && !viewMore && (<div className="view-more-dashboard">
+                    <button onClick={overFlow}>Ver mais</button>
                 </div>
-
+                )}
             </div>
         </main>
-    );
+    )
 }
